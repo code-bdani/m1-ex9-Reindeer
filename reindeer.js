42e72ce3d3e5a2d92f66d321e317db298a332722
Brittany D. Smith <bdaniphotography@gmail.com>
2018-02-03 16:33:45 -0600

    Met MVP for exercise

--- reindeer.js
+++ reindeer.js
@@ -1,1 +1,9 @@
-console.log("Hello, reindeer.js");
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+var hohohoElement = document.getElementById("reindeer");
+
+for (let loopTracker = 0; loopTracker < reindeer.length; loopTracker += 1) {
+    const reindeerWithColors = colors[loopTracker] + " " + reindeer[loopTracker];
+    hohohoElement.innerHTML += `<li>${reindeerWithColors}</li>`;
+}
